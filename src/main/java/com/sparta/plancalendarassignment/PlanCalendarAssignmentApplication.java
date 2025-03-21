36d600c6c1f6792cd9a5889e4c7cd8bbdff249a9
jhyang22 <jaeho9229@gmail.com>
2025-03-21 15:49:06 +0900

    과제 생성

---
+++ src/main/java/com/sparta/plancalendarassignment/PlanCalendarAssignmentApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.plancalendarassignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlanCalendarAssignmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlanCalendarAssignmentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sparta/plancalendarassignment/PlanCalendarAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.plancalendarassignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlanCalendarAssignmentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
